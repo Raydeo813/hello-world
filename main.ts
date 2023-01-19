d3d4ab620424faaf8a81d415b8aa8baaae434475
Raydeo813 <122560231+Raydeo813@users.noreply.github.com>
2023-01-19 13:17:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.TShirt)
+    basic.showIcon(IconNames.Sad)
+    music.playMelody("G E B E C5 D B F ", 130)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Ghost)
+    music.playMelody("F A E B A B C5 A ", 59)
 })
-basic.showIcon(IconNames.Angry)
+basic.showIcon(IconNames.Umbrella)
+for (let index = 0; index < 10; index++) {
+    music.playMelody("G F G F G F G F ", 94)
+}
