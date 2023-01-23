66b3cb5d33092d108464df77efdf05fd48600f1a
Raydeo813 <122560231+Raydeo813@users.noreply.github.com>
2023-01-23 13:15:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,38 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Sad)
-    music.playMelody("G E B E C5 D B F ", 130)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    music.playMelody("B E B E C5 D B F ", 250)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # . . # .
+        `)
+    music.playMelody("E D G F B A C5 B ", 350)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Ghost)
-    music.playMelody("F A E B A B C5 A ", 59)
+    basic.showString("Hi There!")
+    music.playMelody("F A G C5 A B C5 G ", 250)
 })
-basic.showIcon(IconNames.Umbrella)
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("STOP THAT!")
+    music.playMelody("C5 C C5 C C5 C C5 C ", 150)
+})
+basic.showLeds(`
+    . # # # .
+    # . # . #
+    . . # . .
+    # . . . #
+    . # # # .
+    `)
 for (let index = 0; index < 10; index++) {
-    music.playMelody("G F G F G F G F ", 94)
+    music.playMelody("C5 B G C5 B E C5 B ", 250)
 }
